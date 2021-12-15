a8854c6bb435b9cc8e13e2dd215e9ea122290040
Makar <makachino93@gmail.com>
2021-12-15 17:28:40 +0700

    master clean commit

--- src/sagas/authenticationSaga.js
+++ src/sagas/authenticationSaga.js
@@ -1,15 +1,3 @@
-import axios from "axios";
-
-
-export function fetchAuthentication(params, baseUrl, dispatch) {
-  axios.post(`${baseUrl}authentication_token`, params)
-    .then((response) => {
-      console.log("LOG -> .then -> response", response)
-      localStorage.setItem('token', response.data.token);
-      dispatch({type: 'isAuthentication', payload: true});
-    })
-}
-
 export const authProvider = {
   login: ({ username, password }) => {
     const params = {
@@ -18,35 +6,35 @@ export const authProvider = {
     }
 
     const request = new Request('https://api.rating.chgk.net/authentication_token', {
-        method: 'POST',
-        body: JSON.stringify(params),
-        headers: new Headers({ 'Content-Type': 'application/json' }),
+      method: 'POST',
+      body: JSON.stringify(params),
+      headers: new Headers({ 'Content-Type': 'application/json' }),
     });
     return fetch(request)
-        .then(response => {
-            if (response.status < 200 || response.status >= 300) {
-                throw new Error(response.statusText);
-            }
-            return response.json();
-        })
-        .then(({ token }) => {
-            const decodedToken = token;
-            localStorage.setItem('token', token);
-            localStorage.setItem('permissions', decodedToken.permissions);
-        });
-},
-checkError: (error) => { /* ... */ },
-checkAuth: () => {
+      .then(response => {
+        if (response.status < 200 || response.status >= 300) {
+            throw new Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then(({ token }) => {
+        const decodedToken = token;
+        localStorage.setItem('token', token);
+        localStorage.setItem('permissions', decodedToken.permissions);
+      });
+  },
+  checkError: (error) => { /* ... */ },
+  checkAuth: () => {
     return localStorage.getItem('token') ? Promise.resolve() : Promise.reject();
-},
-logout: () => {
+  },
+  logout: () => {
     localStorage.removeItem('token');
     localStorage.removeItem('permissions');
     return Promise.resolve();
-},
-getIdentity: () => { /* ... */ },
-getPermissions: () => {
+  },
+  getIdentity: () => { /* ... */ },
+  getPermissions: () => {
     const role = localStorage.getItem('permissions');
     return role ? Promise.resolve(role) : Promise.reject();
-}
+  }
 };
